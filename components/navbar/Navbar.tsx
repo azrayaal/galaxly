ab9af11455b1ac2c6ed9f70d268075bfa18fcea4
AzraYazid <azrayazidalkautsar@gmail.com>
2023-01-01 13:01:27 +0700

    homepage

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav id="header" className="fixed w-full z-30 top-0 text-white">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <div className="pl-4 flex items-center" style={{ display: 'inline' }}>
+          <a className=" text-white no-underline hover:no-underline text-2xl lg:text-4xl flex" href="#" style={{ verticalAlign: 'baseline' }}>
+            <img src="/galaxly-logo-removebg-preview.png" height={50} width={50} alt="" style={{ verticalAlign: 'baseline' }} className="mr-2" />
+            {/* <p style={{ verticalAlign: 'baseline' }}>galaxly</p> */}
+            <p className="font-thin">
+              galax<span className="font-normal">ly</span>
+            </p>
+          </a>
+        </div>
+        <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent  p-4 lg:p-0 z-20" id="nav-content">
+          <ul className="list-reset lg:flex justify-end flex-1 items-center"></ul>
+          <button className="mx-auto lg:mx-0 font-semibold rounded-xl mt-2 lg:mt-0 py-2 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
+import { Player } from '@lottiefiles/react-lottie-player';
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
+import Script from 'next/script';
+import Navbar from '../components/navbar/Navbar';
 import styles from '../styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,7 +13,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      {/* <nav>a</nav> */}
+      <Navbar />
+      <main className={styles.main}>
+        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+          {/* <!--Left Col--> */}
+          <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left ">
+            <p className="leading-normal">Link Shortner!</p>
+            <h1 className="text-5xl font-bold leading-tight fontcolor1">Make urls </h1>
+            <h1 className="text-5xl font-bold leading-tight"> easy to </h1>
+            <h1 className="mb-4 text-5xl font-bold leading-tight fontcolor2"> Remember!</h1>
+            <p className="leading-normal mb-8">Turn your long and hardread link into beautiful short customisable Link!</p>
+            <button className="mx-auto lg:mx-0 font-semibold rounded-xl my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">Create Link</button>
+          </div>
+          {/* <!--Right Col--> */}
+          <div className="w-full md:w-3/5 text-center">
+            <Script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></Script>
+            <Player src="https://assets6.lottiefiles.com/packages/lf20_P9CLOgR2NG.json" background="transparent" speed={1} style={{ width: '600px', height: '600px' }} loop controls autoplay></Player>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
