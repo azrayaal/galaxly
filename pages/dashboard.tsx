06d09141282e72109807b2d099c48e0d9612d4c4
AzraYazid <azrayazidalkautsar@gmail.com>
2023-01-04 23:33:33 +0700

    update db firebase

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,18 +6,39 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { db } from '../firebase/firebase';
 import Modaladdlink from './components/modals/modaladdlink';
+
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 
 export default function Dashboard() {
   const [showModal, setShowModal] = useState(false);
   const [isLogin, seIsLogin] = useState(false);
   const [url, setUrl] = useState('');
   const [namelink, setNameLink] = useState('');
-  const [loading, setLoading] = useState(false);
-
   const [user, setUser] = useState({
     displayName: '',
   });
+  //
+  const [shortLinks, setShortLinks] = useState<any | null>([]);
+  const shortLinkRef = collection(db, 'shortlink');
+  //
+
+  const getDataShortLinkFromFireBase = async () => {
+    const data = await getDocs(shortLinkRef);
+
+    // const res = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    setShortLinks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+    console.log(
+      'data daridbfb',
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    );
+  };
+
+  useEffect(() => {
+    getDataShortLinkFromFireBase();
+  }, []);
 
   const router = useRouter();
 
@@ -101,54 +122,57 @@ export default function Dashboard() {
             </div>
           </div>
           {/* emd of name */}
-
           {/* card */}
-          <div className=" w-full grid sm:grid-cols-3 grid-cols-1 gap-4 mt-4 ">
-            <div className="sm:mx-12 mx-5 max-w-sm rounded-2xl overflow-hidden shadow-lg bg-transparent border-2 border-gray-50">
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <div className="generated-link my-3">
-                  <p className="text-gray-400 text-base ">Galaxied link:</p>
-                  <p className="text-gray-50 text-base ">
-                    <a href="" target="_blank">
-                      https:babyaga.com
-                    </a>
-                  </p>
+          {shortLinks.map((item: any) => {
+            return (
+              <div className=" w-full grid sm:grid-cols-3 grid-cols-1 gap-4 mt-4" key={item.id}>
+                <div className="sm:mx-12 mx-5 max-w-sm rounded-2xl overflow-hidden shadow-lg bg-slate-700 hover:scale-105 ">
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                    <div className="generated-link my-3">
+                      <p className="text-gray-400 text-base ">Original link:</p>
+                      <p className="text-gray-50 text-base ">
+                        <a href="" target="_blank">
+                          {item.originalLink}
+                        </a>
+                      </p>
+                    </div>
+                    <div className="generated-link my-3">
+                      <p className="text-gray-400 text-base ">Galaxied link:</p>
+                      <p className="text-gray-50 text-base ">
+                        <a href="" target="_blank">
+                          {item.galaxiedLink}
+                        </a>
+                      </p>
+                    </div>
+                    <div className="generated-link my-3">
+                      <p className="text-gray-400 text-base ">id:</p>
+                      <p className="text-gray-50 text-base ">
+                        <a href="" target="_blank">
+                          {item.id}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="px-3 pt-4 pb-2 flex justify-around">
+                    <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAARpJREFUSEvtVdF1gzAMPIUBmg3SBQr2BhkhI9DJygYdodlAQBZINsgCifvUxjyTZxu9EvJV/wHiTrqTLMLChxbGx3MJDsxbR/QB4DVSWVMa8y7ve+Yav3HRUxozJD6qoGvbIwGbjGwqkiRB37ZOwMOAnEepSh5GwMzrgojvJX0IgYCviL4IMA44hdLOJgjBAXQX57YFsPPGzyKIgVtrz2F3/ZkgB+6bQYwvrW3886hNc12kAY91nIpAC953XVNWVR0SqQh6ZjHw0xvqNb/P+KbAMIzyXUVwM3B3AfYp8J+YyKCqCTS37j/BpEoaiWQiXyaRMgFyL1XGDPtkZLIsnCtRM7ETkvACvnKufrN2H53kOZmn/n3uTl6igm/4bcoZlvHtzwAAAABJRU5ErkJggg==" />
+                    </span>
+                    <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAQBJREFUSEvtVbsNwjAQfRfYA0ponEs6poCSEShhAlZgDErGoIqdCImUsAfkUCIF8betkArc5vm9e3e5Z0LLh1rmh1Ug03oiREsC2LGYA0QWKoo2Jd4qkBpzIKDnSF7BBDAhc+QkkBkjPuQ1VjFXxVsdtC5gq15r3e8EwQoi41usswObQPl9pzUXRPqjQJIkYTcI1gAGLqQA8lNRTOM4Tkv8YyufHGTG7D3I6xpyxTx0FWj0t7g4+AvcdeDVkL/ZolQxV9l13eSmG1vfF+BIIvOnsGsq8G53vubgNwT8o4Joq8Jw9Cm7rjPwDjui7el8ntVhZ52BY4J6w6wvmjfjw4ULNXqMGUMmJDAAAAAASUVORK5CYII=" />
+                    </span>
+                    <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPVJREFUSEu9ldERgjAMQBMdQEdwAA/ICE6iTuQI4gaO4AYBzgEcgQU0XjnhBFpMKZjv8l6SJgVh5sCZ+RAkKJgPJsGIKHUlOlpQwRHPFVjk6JKMEjDzeon4AIBVk7lD4i0wcCIq78zJC/HWSBAvURxXLfsOL8GnLaeFyG5LlDUSxKsNbkRqQavnAGUtYeYNEZl2WUMl6MBrUPkUoSG4qgIHHMDRc687KPI8BZF9r3YlfLCCKeBOwVRwq8AJH9jWofesN0VFlknvg5FwewVdQQD8tyAQrtqD0P+FapNDJP8XWKfIo4QoSVpJ68Z0SoEHS3V09jt4A4JofhnQlVrOAAAAAElFTkSuQmCC" />
+                    </span>
+                    <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALVJREFUSEvtldsNwjAMRa9ZhAVQsDdhBNiAEdiEjsAmTisGgEUwX0VQpYlbtfy0+Yysc5ybF2HmQTPzURQ0qkcQXZONmJ2CSJVrMiu4q/KLSHOAjZnsRGJfzY+gidGmiCwwf7j/FUzRfZeR3IOxUX1H04oWIGiX3Y0tNT8qolVQPEVrRAuIaMgD6L5odYwPArZD4ADqwMy+11T1AKILgL1HYsCTzM5B5OYSeKDemuKn7wX11b0Blo+hGbsz/90AAAAASUVORK5CYII=" />
+                    </span>
+                  </div>
                 </div>
-                <div className="generated-link my-3">
-                  <p className="text-gray-400 text-base ">Original link:</p>
-                  <p className="text-gray-50 text-base ">
-                    <a href="" target="_blank">
-                      https:babyaga.com
-                    </a>
-                  </p>
-                </div>
-                <div className="generated-link my-3">
-                  <p className="text-gray-400 text-base ">Created at:</p>
-                  <p className="text-gray-50 text-base ">
-                    <a href="" target="_blank">
-                      24/02/98
-                    </a>
-                  </p>
-                </div>
+                {/* end of card */}
               </div>
-              <div className="px-3 pt-4 pb-2 flex justify-around">
-                <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAARpJREFUSEvtVdF1gzAMPIUBmg3SBQr2BhkhI9DJygYdodlAQBZINsgCifvUxjyTZxu9EvJV/wHiTrqTLMLChxbGx3MJDsxbR/QB4DVSWVMa8y7ve+Yav3HRUxozJD6qoGvbIwGbjGwqkiRB37ZOwMOAnEepSh5GwMzrgojvJX0IgYCviL4IMA44hdLOJgjBAXQX57YFsPPGzyKIgVtrz2F3/ZkgB+6bQYwvrW3886hNc12kAY91nIpAC953XVNWVR0SqQh6ZjHw0xvqNb/P+KbAMIzyXUVwM3B3AfYp8J+YyKCqCTS37j/BpEoaiWQiXyaRMgFyL1XGDPtkZLIsnCtRM7ETkvACvnKufrN2H53kOZmn/n3uTl6igm/4bcoZlvHtzwAAAABJRU5ErkJggg==" />
-                </span>
-                <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAQBJREFUSEvtVbsNwjAQfRfYA0ponEs6poCSEShhAlZgDErGoIqdCImUsAfkUCIF8betkArc5vm9e3e5Z0LLh1rmh1Ug03oiREsC2LGYA0QWKoo2Jd4qkBpzIKDnSF7BBDAhc+QkkBkjPuQ1VjFXxVsdtC5gq15r3e8EwQoi41usswObQPl9pzUXRPqjQJIkYTcI1gAGLqQA8lNRTOM4Tkv8YyufHGTG7D3I6xpyxTx0FWj0t7g4+AvcdeDVkL/ZolQxV9l13eSmG1vfF+BIIvOnsGsq8G53vubgNwT8o4Joq8Jw9Cm7rjPwDjui7el8ntVhZ52BY4J6w6wvmjfjw4ULNXqMGUMmJDAAAAAASUVORK5CYII=" />
-                </span>
-                <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPVJREFUSEu9ldERgjAMQBMdQEdwAA/ICE6iTuQI4gaO4AYBzgEcgQU0XjnhBFpMKZjv8l6SJgVh5sCZ+RAkKJgPJsGIKHUlOlpQwRHPFVjk6JKMEjDzeon4AIBVk7lD4i0wcCIq78zJC/HWSBAvURxXLfsOL8GnLaeFyG5LlDUSxKsNbkRqQavnAGUtYeYNEZl2WUMl6MBrUPkUoSG4qgIHHMDRc687KPI8BZF9r3YlfLCCKeBOwVRwq8AJH9jWofesN0VFlknvg5FwewVdQQD8tyAQrtqD0P+FapNDJP8XWKfIo4QoSVpJ68Z0SoEHS3V09jt4A4JofhnQlVrOAAAAAElFTkSuQmCC" />
-                </span>
-                <span className="bg-slate-700 inline-block rounded-md px-3 py-3 mb-2 text-gray-700">
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALVJREFUSEvtldsNwjAMRa9ZhAVQsDdhBNiAEdiEjsAmTisGgEUwX0VQpYlbtfy0+Yysc5ybF2HmQTPzURQ0qkcQXZONmJ2CSJVrMiu4q/KLSHOAjZnsRGJfzY+gidGmiCwwf7j/FUzRfZeR3IOxUX1H04oWIGiX3Y0tNT8qolVQPEVrRAuIaMgD6L5odYwPArZD4ADqwMy+11T1AKILgL1HYsCTzM5B5OYSeKDemuKn7wX11b0Blo+hGbsz/90AAAAASUVORK5CYII=" />
-                </span>
-              </div>
-            </div>
-            {/* end of card */}
-          </div>
+            );
+          })}
         </main>
 
         {showModal ? <Modaladdlink setShowModal={setShowModal} url={url} setUrl={setUrl} namelink={namelink} setNameLink={setNameLink} handleFormSubmit={handleFormSubmit} /> : null}
