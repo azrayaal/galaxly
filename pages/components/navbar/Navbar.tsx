23805d593f00e441d8ea399e4f483cd882204808
AzraYazid <azrayazidalkautsar@gmail.com>
2023-01-08 22:21:49 +0700

    yupdate

--- pages/components/navbar/Navbar.tsx
+++ pages/components/navbar/Navbar.tsx
@@ -41,7 +41,6 @@ export default function Navbar() {
                 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAXlJREFUSEvtldFxgkAQhv/1fE9Sgdx4+hpTQaQDOzApIR3YQdJBtILYQbAD8io3A3Zg3oXNHCPOgYegGd/kCX5299vbO34IV77oyvXRCIijaNwhmjLRCMyjvCGikJjDjHkhB4PgVJO1gHi9HlGn8w5g3LDKgLPsTQ6HoSvOCdgX/wZw33KEW84y3wU5AlxQvOjBCTkCJFqbmT637LwaFnhK+bZYApgNJSIzmosvZvbtjS8BNlrPGZha1VcsxEt+cNJ0bq2sTjfHctFTKs/J8+xWkygKQfRYaCyElFIm5jmOY4/SNDb3dXpxhL1+/8kN0JptIAvxIKXcOgBOvcj1lDo0Xl5BBQAgYCFe9yP6tL6JOj1nnAO4aLNvgMax3Ub0jxFVvuTGSu6AlafU4R9SNjutJwR8OfJ+AdxVdJeGk2aXW4KBMM/2nrRhYAYhAtrtlgefYv7hbneCNB2TeQ/0AKwY+JBKLWvtumkkxvBMTGGATfFHbtom4dyYP8B7wxmaFaV8AAAAAElFTkSuQmCC"
               />{' '}
               <span>{user.displayName}</span>
-              <span>{user.displayName}</span>
             </button>
           </Link>
         </div>
@@ -52,6 +51,13 @@ export default function Navbar() {
       <nav id="header" className="fixed w-full z-30 top-0 text-white">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center" style={{ display: 'inline' }}>
+            <a className=" text-white no-underline hover:no-underline text-2xl lg:text-4xl flex" href="#" style={{ verticalAlign: 'baseline' }}>
+              <Image src="/galaxly-logo-removebg-preview.png" height={50} width={50} alt="" style={{ verticalAlign: 'baseline' }} className="mr-2" />
+              {/* <p style={{ verticalAlign: 'baseline' }}>galaxly</p> */}
+              <p className="font-thin">
+                galax<span className="font-normal">ly</span>
+              </p>
+            </a>
             <a className=" text-white no-underline hover:no-underline text-2xl lg:text-4xl flex" href="#" style={{ verticalAlign: 'baseline' }}>
               <Image src="/galaxly-logo-removebg-preview.png" height={50} width={50} alt="" style={{ verticalAlign: 'baseline' }} className="mr-2" />
               {/* <p style={{ verticalAlign: 'baseline' }}>galaxly</p> */}
